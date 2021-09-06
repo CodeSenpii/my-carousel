b0843c60e5dab64007d37d24590862cc34f622fc
kieyn.parks <kieyn.parks@snhu.edu>
2021-09-06 17:46:31 -0400

    changes to images to Divs

--- index.js
+++ index.js
@@ -2,4 +2,42 @@
 const track = document.querySelector('.carousel__track');
 const slides = Array.from(track.children);
 const nextButton = document.querySelector('.carousel__button--right');
-const prevButton = document.querySelector('carousel__button--left');
+const prevButton = document.querySelector('.carousel__button--left');
+const dotsNav = document.querySelector('.carousel__nav');
+const dots = Array.from(dotsNav.children);
+const slideSize = slides[0].getBoundingClientRect();// dimension of the slide is importatnt
+console.log(slideSize.width * 0 + "px");
+console.log(slideSize);
+const slideWidth = slideSize.width;
+
+
+// arrage slides next to each other
+// slides[0].style.left = slideWidth * 0 + 'px';
+// slides[1].style.left = slideWidth * 1 + 'px';
+// slides[2].style.left = slideWidth * 2 + 'px';
+
+// // for loop
+// for (var x = 0; x<slides.length; x++){
+//   slides[x].style.left = slideWidth * x + 'px';
+// }
+
+// for each loop
+
+slides.forEach((slide, index) => {
+  slide.style.left = slideWidth * index + 'px';
+});
+
+
+
+nextButton.addEventListener('click', (e) => {
+  // move slides
+  const currentSlide = track.querySelector('.current-slide');
+  const nextSlide = currentSlide.nextElementSibling;
+
+  //move to next file
+  const amountToMove = nextSlide.style.left; // get value
+
+  track.style.transform = 'translateX(-' + amountToMove + ')';
+  currentSlide.classList.remove('current-slide');
+  nextSlide.classList.add('current-slide');
+});
